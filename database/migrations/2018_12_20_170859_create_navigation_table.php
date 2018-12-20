e8685d0ee5608e33cf34d336f35837088e460677
amir.altmyshbaev <amir.altmyshbaev@lightspeedhq.com>
2018-12-20 16:31:02 -0500

    ewe

---
+++ database/migrations/2018_12_20_170859_create_navigation_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNavigationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('navigation', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_id');
+            $table->string('name');
+            $table->string('link');
+            $table->string('locale');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('navigation');
+    }
+}
